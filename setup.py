dce79af9697b5cc1552dbdb894d566d2c6d2a3e3
Napat Sahapat <n.sahapat@outlook.com>
2025-05-06 13:32:59 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="snn",
+    version="0.1.0",
+    author="Napat Sahapat",
+    description="Simple Spiking Neural Network (SNN) library with learnable synapses",
+    packages=["snn"]
+)
