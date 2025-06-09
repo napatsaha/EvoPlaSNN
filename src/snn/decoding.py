0f487f85dbbe02ea73169d91654758759dff4436
Napat Sahapat <n.sahapat@outlook.com>
2025-06-09 18:08:53 +1000

    Added a few new basic Decoders: final step, rate and latency

--- src/snn/decoding.py
+++ src/snn/decoding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Literal, Protocol, override
 
 
 class RewardManager:
@@ -40,3 +41,162 @@ class RewardManager:
         Reset the reward manager.
         """
         self.memory = {"t": [], "label": [], "prediction": [], "reward": []}
+
+
+class Decoder(Protocol):
+    def record(self, spikes: np.ndarray) -> None:
+        pass
+
+    def calculate_reward(self, target) -> float:
+        pass
+
+    def reset(self) -> None:
+        pass
+
+
+class BaseDecoder(Decoder):
+    def __init__(self, buffer_size: int, neuron_size: int):
+        super().__init__()
+        self.buffer_size = buffer_size
+        self.neuron_size = neuron_size
+        self.buffer = np.zeros((self.neuron_size, self.buffer_size), dtype=np.int_)
+        self.reward_buffer = []
+        self.count = 0
+        self._full = False
+        self.reward_null = 0.0
+
+    def reset(self) -> None:
+        """
+        Reset the decoder buffer and count.
+        """
+        self.buffer.fill(0)
+        self._full = False
+        self.reward_buffer.clear()
+        self.count = 0
+
+    def record(self, spikes: np.ndarray) -> None:
+        """
+        Record spikes in the buffer.
+        """
+        if spikes.ndim != 1 or spikes.shape[0] != self.neuron_size:
+            raise ValueError(f"Expected spikes shape ({self.neuron_size},), got {spikes.shape}")
+        
+        self.buffer[:, self.count] = spikes
+        self.count += 1
+        if self.count == self.buffer_size:
+            self._full = True
+        else:
+            self._full = False
+        self.count %= self.buffer_size
+
+    def decode(self) -> int | None:
+        """
+        Decode the buffer into predicted class. 
+        If tied, return None.
+        """
+        if not self._full:
+            raise ValueError("Buffer is not full. Cannot decode.")
+        
+        a = self._decode()
+        # Check if there is a tie
+        if len(np.unique(a)) == 1:
+            return None
+        # Otherwise return neuron with largest value
+        else:
+            return np.argmax(a)
+
+    def _decode(self) -> np.ndarray:
+        """
+        Converts the buffer into a scalar for each output neuron.
+        Subclass must implement this method.
+        """
+        raise NotImplementedError("Subclasses should implement this method.")
+    
+    def calculate_reward(self, label: int) -> float:
+        """
+        Calculate the reward based on the decoded output and given target label.
+        """
+        pred = self.decode()
+        if pred is None:
+            reward = self.reward_null
+        else:
+            reward = 1.0 if pred == label else -1.0
+
+        self.reward_buffer.append(reward)
+        return reward
+    
+    def get_fitness(self) -> float:
+        """
+        Calculate the fitness based on accumulated rewards.
+        """
+        if not self.reward_buffer:
+            raise ValueError("No rewards recorded. Cannot calculate fitness.")
+        return np.mean(self.reward_buffer)
+
+
+class FinalStepDecoder(BaseDecoder):
+    """
+    A Decoder that only uses information about the final step of a sequence.
+    """
+    def __init__(self, buffer_size, neuron_size):
+        super().__init__(buffer_size, neuron_size)
+    
+    @override
+    def _decode(self) -> np.ndarray:
+        """
+        Returns the last recorded spike train.
+        """
+        return self.buffer[:, -1]
+
+
+class RateDecoder(BaseDecoder):
+    def __init__(self, buffer_size, neuron_size):
+        super().__init__(buffer_size, neuron_size)
+
+    @override
+    def _decode(self) -> np.ndarray:
+        """
+        Count number of spikes in each neuron.
+        """
+        return np.sum(self.buffer, axis=1)
+    
+
+class LatencyDecoder(BaseDecoder):
+    def __init__(self, buffer_size, neuron_size, direction: Literal["first", "last"] = "first",
+                 convert_type: Literal["negate", "invert"] = "negate"):
+        super().__init__(buffer_size, neuron_size)
+        if direction not in ["first", "last"]:
+            raise ValueError("Direction must be 'first' or 'last'.")
+        if convert_type not in ["negate", "invert"]:
+            raise ValueError("Convert type must be 'negate' or 'invert'.")
+        self.direction = direction
+        self.convert_type = convert_type
+
+    @override
+    def _decode(self) -> np.ndarray:
+        if self.direction == "last":
+            a = np.flip(self.buffer, axis=1)
+        else:
+            a = self.buffer
+        # Find time to first spike for each neuron
+        ttfs = np.argmax(a, axis=1)
+
+        # Finally, convert to opposite
+        if self.convert_type == "negate":
+            return -ttfs
+        elif self.convert_type == "invert":
+            return np.where(ttfs == 0, 0, 1 / ttfs)
+        else:
+            raise ValueError("Invalid convert type. Must be 'negate' or 'invert'.")
+
+
+
+def softmax(x):
+    e_x = np.exp(x)
+    return e_x / e_x.sum()
+
+def cross_entropy(p, q):
+    return np.sum(p * np.log(q + 1e-10))
+
+def mean_square_error(p, q):
+    return np.sum((p - q) ** 2)
