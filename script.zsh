3f19cb09e6a31a3e33a4843ca2a5370851752766
Napat Sahapat <n.sahapat@outlook.com>
2025-07-11 15:49:07 +1000

    Minor changes to script.zsh

--- script.zsh
+++ script.zsh
@@ -1,4 +1,6 @@
 run_name="25-07-10_weight-and-bias"
+mkdir results/binary_es/$run_name
+cp script.zsh results/binary_es/$run_name/script.zsh
 
 bias=(true false)
 use_weights=(true false)
@@ -8,7 +10,7 @@ reward_type=(weighted mapped)
 for b in "${bias[@]}"; do
   for w in "${use_weights[@]}"; do
     echo "Running with bias=$b and use_weights=$w"
-    python src/run_evo_lrule.py --parent $run_name --config binary_es_v2_$reward_type.yaml --override arule_params.use_weights=$w arule_params.bias=$b
+    python src/run_evo_lrule.py --parent $run_name --config binary_es_v2_$reward_type.yaml --override arule_params.use_weights=$w arule_params.bias=$b arule_params.learning_rate=0.1
   done
 done
 # python src/run_evo_lrule.py --parent $run_name --config binary_es_v2_weighted.yaml --override arule_params.use_weights=True arule_params.bias=True
