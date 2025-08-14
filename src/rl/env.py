ed8869d64df8a9f7a079c8a3137a1d4416921e4b
Napat Sahapat <n.sahapat@outlook.com>
2025-08-14 19:29:33 +1000

    TMaze action handling, reset() and step()

--- src/rl/env.py
+++ src/rl/env.py
@@ -7,6 +7,7 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from typing import Tuple
 
 
 class TMaze(gym.Env):
@@ -15,7 +16,21 @@ class TMaze(gym.Env):
     AGENT = 2
     REWARD = 3
 
-    def __init__(self, size=None, width=None, height=None, pad=1):
+    action_map = {
+        0: np.array([-1, 0], dtype=np.int8),  # Up
+        1: np.array([1, 0], dtype=np.int8),   # Down
+        2: np.array([0, -1], dtype=np.int8),  # Left
+        3: np.array([0, 1], dtype=np.int8)    # Right
+    }
+
+    action_names = {
+        0: "Up",
+        1: "Down",
+        2: "Left",
+        3: "Right"
+    }
+
+    def __init__(self, size=None, width=None, height=None, pad=1, max_steps=50):
         super().__init__()
         self.width = size if width is None else width
         self.height = size if height is None else height
@@ -23,10 +38,11 @@ class TMaze(gym.Env):
         self.action_space = gym.spaces.Discrete(4)
         self._create_maze()
         self.observation_space = gym.spaces.Discrete(self._num_state)
+        self.max_steps = max_steps
+        self._step_count = 0
 
-        # Set up plotting variables and Enums
+        # Set up plotting variables
         self.cmap = colors.ListedColormap([ "white", "black","blue", "green"])
-
 
     def _create_maze(self):
         # Create empty array (filled with walls, as 1's)
@@ -48,7 +64,15 @@ class TMaze(gym.Env):
         self._empty_idx = np.flatnonzero(self.maze)
         self._agent_pos = np.argwhere(self.maze == self.AGENT)[0]
         self._reward_pos = np.argwhere(self.maze == self.REWARD)[0]
+        self._starting_state = self._convert_pos_to_state(self._agent_pos)
         self._state_pos_dict = {state: self._convert_state_to_pos(state) for state in range(self._num_state)}
+
+    def _reset_position(self):
+        self.maze[np.unravel_index(self._empty_idx, self.maze.shape)] = self.EMPTY
+        self.maze[*self._reward_pos] = self.REWARD
+        self._agent_pos = self._convert_state_to_pos(self._starting_state)
+        self.maze[*self._agent_pos] = self.AGENT
+        self._step_count = 0
 
     def render(self, fig_scale=1.0):
         fig, ax = plt.subplots(figsize=(self.width * fig_scale, self.height * fig_scale))
@@ -59,6 +83,29 @@ class TMaze(gym.Env):
         ax.set_yticks(np.arange(0, self.height, 1), labels=[])
         ax.grid(visible=True, color='gray', linewidth=1)
         plt.show()
+        # return fig
+    
+    def reset(self, *, seed = None, options = None):
+        super().reset(seed=seed, options=options)
+        self._step_count = 0
+        self._reset_position()
+        state = self.get_agent_position()
+        info = {
+            'step_count': self._step_count,
+            'reward_position': self.get_reward_position()
+        }
+        return state, info
+
+    def step(self, action: int) -> Tuple[int, float | None, bool, bool, dict]:
+        self._step_count += 1
+        truncated = self._step_count >= self.max_steps
+        reward, terminated = self._take_action(action)
+        state = self.get_agent_position()
+        info = {
+            'step_count': self._step_count,
+            'reward_position': self.get_reward_position()
+        }
+        return state, reward, terminated, truncated, info
 
     def get_maze(self):
         return self.maze.copy()
@@ -72,12 +119,38 @@ class TMaze(gym.Env):
     def _convert_pos_to_state(self, pos: np.ndarray):
         flat_idx = np.ravel_multi_index(pos, self.maze.shape)
         state_idx = np.isin(self._empty_idx, flat_idx).nonzero()[0]
-        return state_idx
+        return state_idx.item()
     
     def _convert_state_to_pos(self, state: int):
         if state < 0 or state >= self._num_state:
             raise ValueError(f"State index out of bounds. Must be between [0, {self._num_state - 1}]. Got state: {state}")
         flat_idx = self._empty_idx[state]
         pos = np.unravel_index(flat_idx, self.maze.shape)
-        return np.asarray(pos)
+        return np.asarray(pos).reshape(2)
 
+    def _take_action(self, action: int) -> Tuple[float | None, bool]:
+        if action < 0 or action >= self.action_space.n:
+            raise ValueError(f"Invalid action: {action}. Must be between [0, {self.action_space.n - 1}].")
+        
+        reward = None
+        terminated = False
+
+        # Perform movement
+        new_pos = self._agent_pos + self.action_map[action]
+        new_item = self.maze[*new_pos]
+
+        # Check where the agent would end up
+        if new_item == self.WALL:
+            pass
+        elif new_item == self.EMPTY:
+            self.maze[tuple(self._agent_pos)] = self.EMPTY
+            self._agent_pos = new_pos
+            self.maze[tuple(self._agent_pos)] = self.AGENT
+        elif new_item == self.REWARD:
+            self.maze[tuple(self._agent_pos)] = self.EMPTY
+            self._agent_pos = new_pos
+            self.maze[tuple(self._agent_pos)] = self.AGENT
+            reward = 1.0
+            terminated = True
+        
+        return reward, terminated
