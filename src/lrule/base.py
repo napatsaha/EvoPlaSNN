32c611680c3c1553291f404f7560b0adfa74914a
Napat Sahapat <n.sahapat@outlook.com>
2026-09-03 16:42:23 +1000

    v1.4.0.0 -- Change delig to multiply with instead of add learning_rate in BaseLearningRule

--- src/lrule/base.py
+++ src/lrule/base.py
@@ -129,7 +129,7 @@ class BaseLearningRule(LearningRule):
             idx += 1
         if self.delta_eligibility:
             delig = out[..., idx]
-            delig += self.learning_rate
+            delig *= self.learning_rate
             idx += 1
 
         # Return values -- always return tuples for consistency
